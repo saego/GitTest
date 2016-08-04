1ae87319c0cf1b2708912e8a2efa52ee12925df8
Saego <usonikru@gmail.com>
2016-08-04 23:55:41 +0300

    Calculator

---
+++ 2/src/main/java/calc/Calculator.java
@@ -0,0 +1,20 @@
+package calc;  
+
+public class Calculator{
+	double result;
+	public void add(double first, double second){
+		this.result = first + second;
+	}
+	public void substruct(double first, double second){
+		this.result = first - second;
+	}
+	public void multiple(double first, double second){
+		this.result = first * second;
+	}
+	public void div(double first, double second){
+		this.result = first / second;
+	}
+	public double out(){
+	return result;
+	}
+}
