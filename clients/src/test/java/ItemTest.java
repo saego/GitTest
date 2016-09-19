56c1545d0d2959da5cdd233867438cd1220aa7c3
androsdav <androsdav@bigmir.net>
2016-09-19 13:36:08 +0300

    Item done

---
+++ clients/src/test/java/ItemTest.java
@@ -0,0 +1,50 @@
+import org.junit.Assert;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Created by ${Ruslan} on 19.09.16.
+ */
+public class ItemTest {
+    String clientName = "Viktor";
+    long dateAdd = 32;
+    String description = "repair my TV pls";
+        Item item = new Item(clientName, dateAdd, description);
+
+    @org.junit.Test
+    public void testSetName() throws Exception {
+        String testName = "Nick";
+        this.item.setName(testName);
+        assertThat(item.getName(), is(testName));
+    }
+
+    @org.junit.Test
+    public void testSetDateAdd() throws Exception {
+        long testDateAdd = 48;
+        this.item.setDateAdd(testDateAdd);
+        assertThat(item.getDateAdd(),is(testDateAdd));
+    }
+
+    @org.junit.Test
+    public void testSetDescription() throws Exception {
+        String testDescription = "repair my iphone";
+        this.item.setDescription(testDescription);
+        assertThat(item.getDescription(),is(testDescription));
+    }
+
+    @org.junit.Test
+    public void testGetName() throws Exception {
+        assertThat(item.getName(), is(clientName));
+    }
+
+    @org.junit.Test
+    public void testGetDateAdd() throws Exception {
+        assertThat(item.getDateAdd(), is(dateAdd));
+    }
+
+    @org.junit.Test
+    public void testGetDescription() throws Exception {
+        assertThat(item.getDescription(), is(description));
+    }
+}
