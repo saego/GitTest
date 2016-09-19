e2581687a8035c8ea4e242472f9386fa7bc32385
androsdav <androsdav@bigmir.net>
2016-09-19 11:07:48 +0300

    rrr

---
+++ clients/src/main/java/Item.java
@@ -0,0 +1,38 @@
+/**
+ * Created by ${Ruslan} on 19.09.16.
+ */
+public class Item {
+    String name;
+    long dateAdd;
+    String description;
+
+    public Item(String name, long dateAdd, String description) {
+        this.name = name;
+        this.dateAdd = dateAdd;
+        this.description = description;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDateAdd(long dateAdd) {
+        this.dateAdd = dateAdd;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public long getDateAdd() {
+        return dateAdd;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+}
