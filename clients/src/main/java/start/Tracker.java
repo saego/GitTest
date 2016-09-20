2c15697766c82f35359b22e69712d4be39f123a9
androsdav <androsdav@bigmir.net>
2016-09-20 11:54:59 +0300

    add && findId

--- clients/src/main/java/start/Tracker.java
+++ clients/src/main/java/start/Tracker.java
@@ -6,11 +6,23 @@ import moduls.Item;
  * Created by ${Ruslan} on 19.09.16.
  */
 public class Tracker {
-    private Item[] item = new Item[20];
+    private Item[] items = new Item[20];
     private int position = 0;
 
     public Item addClient(Item item){
-        this.item[position++] = item;
+        this.items[position++] = item;
         return item;
+    }
+
+    public Item findById(String clientId){
+        Item res = null;
+        for (Item item:
+             items) {
+            if (item != null && item.getClientId().equals(clientId)){
+                res = item;
+                break;
+            }
+        }
+        return res;
     }
 }
