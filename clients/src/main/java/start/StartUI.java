9fee7e4782ea6cd60ea473257aa9ccd9af257047
androsdav <androsdav@bigmir.net>
2016-10-04 16:57:55 +0300

    16/57 04/10 Start UI is not finished

--- clients/src/main/java/start/StartUI.java
+++ clients/src/main/java/start/StartUI.java
@@ -1,10 +1,14 @@
 package start;
 
+import moduls.*;
+
 /**
- * Created by ${Ruslan} on 04.10.16.
+ Created by ${Ruslan} on 04.10.16.
+
  */
 public class StartUI {
+    Tracker tracker = new Tracker();
     public static void main(String[] args){
-
+        StartUI start = new StartUI();
     }
 }
