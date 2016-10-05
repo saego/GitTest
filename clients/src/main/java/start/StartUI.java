ac531fd44c5ec5486dec33a54c25e7f3160fb191
androsdav <androsdav@bigmir.net>
2016-10-05 13:17:00 +0300

    13/16 05/10 Start UI is not finished

--- clients/src/main/java/start/StartUI.java
+++ clients/src/main/java/start/StartUI.java
@@ -14,22 +14,25 @@ public class StartUI {
     }
         public void addClients(){
 
-        Item[] items = new Item[5];
+        Item[] items = new Item[6];
         items[0] = new Item("Oxana", 1, "description 1");
         items[1] = new Item("Owen", 2, "description 2");
         items[2] = new Item("Mike", 3, "description 3");
         items[3] = new Item("Nick", 4, "description 4");
         items[4] = new Item("Mike", 5, "description 5");
+        items[5] = new Item("Johnatan", 6, "description 6");
 
             for (Item item:
                  items) {
                 tracker.addClient(item);
             }
             System.out.println("________* Client Base *________");
+            System.out.println();
             for (Item trackerItems:
                  tracker.getAll()) {
                 if(trackerItems != null){
                     System.out.println("Client Id: " + trackerItems.getClientId() + "   Client name: " + trackerItems.getName() + "   Date add: " + trackerItems.getDateAdd() + "   Description: " + trackerItems.getDescription());
+                    System.out.println("*-*-*");
                 }
             }
         }
