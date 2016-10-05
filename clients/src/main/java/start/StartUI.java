ef03344358c3ba2f7f809a23d0f4238472a6d27c
androsdav <androsdav@bigmir.net>
2016-10-05 13:31:24 +0300

    13/31 05/10 Start UI is not finished

--- clients/src/main/java/start/StartUI.java
+++ clients/src/main/java/start/StartUI.java
@@ -11,29 +11,39 @@ public class StartUI {
     public static void main(String[] args) {
         StartUI start = new StartUI();
         start.addClients();
+        start.searchClients();
     }
-        public void addClients(){
+        private void addClients(){
 
-        Item[] items = new Item[6];
+            Item[] items = new Item[6];
         items[0] = new Item("Oxana", 1, "description 1");
         items[1] = new Item("Owen", 2, "description 2");
         items[2] = new Item("Mike", 3, "description 3");
         items[3] = new Item("Nick", 4, "description 4");
         items[4] = new Item("Mike", 5, "description 5");
-        items[5] = new Item("Johnatan", 6, "description 6");
+        items[5] = new Item("Johnathan", 6, "description 6");
 
+            System.out.println("                                        ______/Add clients and show all clients/______");
             for (Item item:
                  items) {
                 tracker.addClient(item);
             }
+            System.out.println();
             System.out.println("________* Client Base *________");
             System.out.println();
             for (Item trackerItems:
                  tracker.getAll()) {
                 if(trackerItems != null){
-                    System.out.println("Client Id: " + trackerItems.getClientId() + "   Client name: " + trackerItems.getName() + "   Date add: " + trackerItems.getDateAdd() + "   Description: " + trackerItems.getDescription());
+                    System.out.println("Client Id: " + trackerItems.getClientId()
+                            + "   Client name: " + trackerItems.getName()
+                            + "   Date add: " + trackerItems.getDateAdd()
+                            + "   Description: " + trackerItems.getDescription());
                     System.out.println("*-*-*");
                 }
             }
+        }
+
+        private void searchClients(){
+
         }
 }
