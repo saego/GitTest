413cecdc7de8b2a0679f9500796b7506318abaab
androsdav <androsdav@bigmir.net>
2016-10-07 14:46:27 +0300

    07/10 14/45 menu

--- clientsConsole/src/main/java/start/StartUIConsole.java
+++ clientsConsole/src/main/java/start/StartUIConsole.java
@@ -6,7 +6,8 @@ import java.util.Date;
 import java.util.Scanner;
 
 /**
- * Created by ${Ruslan} on 07.10.16.
+ Created by ${Ruslan} on 07.10.16.
+
  */
 public class StartUIConsole {
 
@@ -31,7 +32,21 @@ public class StartUIConsole {
         public Scanner scanner = new Scanner(System.in);
 
         public void menu(){
-
+            System.out.println("***MENU***");
+            System.out.println();
+            System.out.println("1: Add new client");
+            System.out.println();
+            System.out.println("2: Show all clients");
+            System.out.println();
+            System.out.println("3: Update client");
+            System.out.println();
+            System.out.println("4: Search client by Id");
+            System.out.println();
+            System.out.println("5: Remove client by Id");
+            System.out.println();
+            System.out.println("6: Add comment by client Id");
+            System.out.println();
+            System.out.println("7: Exit");
         }
 
         public void add(){
