ad8c613ad0ee1b4ba461376de2af36f84d7b92fe
Saego <usonikru@gmail.com>
2016-12-22 16:15:27 +0200

    22/12/2016 16/15 knight not finished

---
+++ Chess/src/main/java/Knight.java
@@ -0,0 +1,17 @@
+/**
+ Created by ${Ruslan} on 22.12.16.
+ */
+public class Knight extends Figure{
+    public Knight(String colour, Cell cell) {
+        super(colour, cell);
+    }
+
+    @Override
+    public Cell[] way(Cell newPosition) throws ImpossibleToMoveException {
+        boolean invalid = true;
+        if (){
+            invalid = false;
+        }
+        return new Cell[0];
+    }
+}
