8facac6209149c3abd519e68c7d35710205fa281
Saego <usonikru@gmail.com>
2016-12-22 17:25:35 +0200

    22/12/2016 16/15 knight without tests

--- Chess/src/main/java/GoPlay.java
+++ Chess/src/main/java/GoPlay.java
@@ -8,6 +8,7 @@ public class GoPlay {
         chessBoard.addFigure(new Bishop("black", new Cell(3, 6)));
         chessBoard.addFigure(new Castle("white", new Cell(5, 4)));
         chessBoard.addFigure(new Queen("black", new Cell(4, 6)));
+        chessBoard.addFigure(new Knight("white", new Cell(7, 3)));
         System.out.println("Figures on the board: ");
         for (Figure figure :
                 chessBoard.getFigures()) {
@@ -18,8 +19,8 @@ public class GoPlay {
             }
         }
         System.out.println("MOVING!");
-        Cell source = new Cell(4, 6);
-        Cell pointDestination = new Cell(4, 1);
+        Cell source = new Cell(7, 3);
+        Cell pointDestination = new Cell(8, 5);
         try {
             chessBoard.move(source, pointDestination);
         for (Figure figure :
--- Chess/src/main/java/Knight.java
+++ Chess/src/main/java/Knight.java
@@ -1,17 +1,29 @@
+import static java.lang.Math.abs;
+
 /**
  Created by ${Ruslan} on 22.12.16.
  */
 public class Knight extends Figure{
-    public Knight(String colour, Cell cell) {
+    Knight(String colour, Cell cell) {
         super(colour, cell);
     }
 
     @Override
     public Cell[] way(Cell newPosition) throws ImpossibleToMoveException {
         boolean invalid = true;
-        if (){
+        if ((abs(this.position.getPositionH() - newPosition.getPositionH()) != 0) &&
+                (abs(this.position.getPositionV() - newPosition.getPositionV()) != 0) &&
+                ((abs(this.position.getPositionH() - newPosition.getPositionH()) + abs(this.position.getPositionV() - newPosition.getPositionV())) == 3)){
             invalid = false;
         }
-        return new Cell[0];
+        if (invalid){
+         throw new ImpossibleToMoveException("Knight can't move this way");
+        }
+        else {
+            int pointQuantity = 1;
+            Cell []wayPoints = new Cell[pointQuantity];
+            wayPoints[0] = new Cell(newPosition.getPositionH(), newPosition.getPositionV());
+            return wayPoints;
+        }
     }
 }
