be5b1b14fc10d15c7e753c2bf854a8ec8be016c9
Saego <usonikru@gmail.com>
2016-12-26 17:06:00 +0200

    26/12/2016 17/15 start streams

---
+++ PairData/src/main/java/Stream.java
@@ -0,0 +1,12 @@
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+/**
+ Created by ${Ruslan} on 26.12.16.
+ */
+public class Stream {
+    public static void main(String []args){
+        BufferedReader br = new BufferedReader(new InputStream in);
+    }
+}
