19778de2eab5b184580b9cdf424965aeaf11f12d
Saego <usonikru@gmail.com>
2016-12-27 17:40:50 +0200

    27/12/2016 17/40 start streams

--- PairData/src/main/java/Stream.java
+++ PairData/src/main/java/Stream.java
@@ -1,4 +1,5 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 
@@ -6,10 +7,14 @@ import java.io.InputStreamReader;
  Created by ${Ruslan} on 26.12.16.
  */
 public class Stream {
-    public static void main(String []args){
-        //BufferedReader br = new BufferedReader(new InputStream in);
-        public boolean isNumber(InputStream in){
-
+        public boolean isNumber(InputStream in) throws IOException{
+            boolean isDivByTwo = false;
+                BufferedReader br = new BufferedReader(new InputStreamReader(in));
+                String val = br.readLine();
+                System.out.println(val);
+                if (Integer.valueOf(val) % 2 == 0){
+                    isDivByTwo = true;
+                }
+            return isDivByTwo;
         }
-    }
 }
---
+++ PairData/src/test/java/StreamTest.java
@@ -0,0 +1,18 @@
+import java.io.ByteArrayInputStream;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.*;
+
+/**
+ Created by ${Ruslan} on 27.12.16.
+ */
+public class StreamTest {
+    @org.junit.Test
+    public void isNumber() throws Exception {
+    ByteArrayInputStream testStream = new ByteArrayInputStream("12 28".getBytes());
+        Stream stream = new Stream();
+        //stream.isNumber(testStream);
+        assertThat(stream.isNumber(testStream), is(true));
+    }
+
+}
