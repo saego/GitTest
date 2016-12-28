3730388cff44ccc9abea8b3f15a90f4594938c62
Saego <usonikru@gmail.com>
2016-12-28 17:31:29 +0200

    28/12/2016 17/31 abuse started

---
+++ Abuse/src/main/java/Abuse.java
@@ -0,0 +1,13 @@
+import java.io.*;
+
+/**
+ * Created by ${Ruslan} on 28.12.16.
+ */
+public class Abuse {
+    public void dropAbuses(InputStream in, OutputStream out, String []abuse) throws IOException {
+        InputStreamReader sr = new InputStreamReader(in);
+        OutputStreamWriter ow = new new OutputStreamWriter(out);
+        char word = sr.read();
+    }
+}
+
