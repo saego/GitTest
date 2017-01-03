94996cb9ab2a7db28ed246e6e916650e719ce902
Saego <usonikru@gmail.com>
2017-01-03 16:42:47 +0200

    03/01/2017  16/42 sort 3g

--- Sort3G/src/main/java/SortFile.java
+++ Sort3G/src/main/java/SortFile.java
@@ -1,8 +1,10 @@
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 /**
  Created by ${Ruslan} on 03.01.17.
  */
 public interface SortFile {
-    public void sort(File source, File destination);
+    public void sort(File source, File destination) throws IOException;
 }
