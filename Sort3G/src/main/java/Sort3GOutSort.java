a6ef24d924c7905cceb5582235a4a6d2fc5c17bf
Saego <usonikru@gmail.com>
2017-01-04 17:44:51 +0200

    04/01/2017  16/49

--- Sort3G/src/main/java/Sort3GOutSort.java
+++ Sort3G/src/main/java/Sort3GOutSort.java
@@ -13,8 +13,21 @@ public class Sort3GOutSort implements SortFile {
         String line1;
         while ((line1 = br.readLine()) != null){
             System.out.println(line1);
-            bw.write(line1);
-            rows++;
+            line2 = br.readLine();
+            if (line1.length() > line2.length()) {
+                bw.write(line2);
+                bw.newLine();
+                bw.write(line1);
+                bw.newLine();
+                rows++;
+            }
+            else {
+                bw.write(line1);
+                bw.newLine();
+                bw.write(line2);
+                bw.newLine();
+                rows++;
+            }
         }
         System.out.println(rows);
         br.close();
