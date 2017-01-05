fbe25f99c5aec257d631cc7e9c053e293aa558f6
Saego <usonikru@gmail.com>
2017-01-05 16:53:54 +0200

    05/01/2017  16/53

--- Sort3G/src/main/java/Sort3GOutSort.java
+++ Sort3G/src/main/java/Sort3GOutSort.java
@@ -10,22 +10,22 @@ public class Sort3GOutSort implements SortFile {
 //        BufferedWriter bw = new BufferedWriter(new FileWriter(destination));
 
         //рахуємо кількість рядків
+        int rows = 0;
         try(BufferedReader br = new BufferedReader(new FileReader(source))) {
-            int rows = 0;
             while (br.readLine() != null) {
                 rows++;
             }
-        System.out.println(rows);
         }
 
         //створюємо тимчасовий файл
-        File tmp = new File("tmp.txt");
+        File tmp1 = new File("tmp1.txt");
+        File tmp2 = destination;
+        //записуємо найдовший рядок в файлі джерела останнім в тимчасовому файлі
         String line;
         String min, max;
         BufferedReader br1 = new BufferedReader(new FileReader(source));
-        //записуємо найдовший рядок в файлі джерела останнім в тимчасовому файлі
         String buffer = br1.readLine();
-        BufferedWriter bw1 = new BufferedWriter(new FileWriter(tmp));
+        BufferedWriter bw1 = new BufferedWriter(new FileWriter(tmp1));
         while ((line = br1.readLine()) != null){
             if (line.length() > buffer.length()){
                 min = buffer;
@@ -42,6 +42,39 @@ public class Sort3GOutSort implements SortFile {
         bw1.write(buffer);
         bw1.close();
         br1.close();
+        //продовжуємо канітєлі
+        File src, dst;
+        for (int k = 1; k <= rows; k++) {
+            if (k % 2 == 0){
+                src = tmp2;
+                dst = tmp1;
+            }
+            else {
+                src = tmp1;
+                dst = tmp2;
+            }
+            BufferedReader br2 = new BufferedReader(new FileReader(src));
+            buffer = br2.readLine();
+            BufferedWriter bw2 = new BufferedWriter(new FileWriter(dst));
+            while ((line = br2.readLine()) != null) {
+                if (line.length() > buffer.length()) {
+                    min = buffer;
+                    max = line;
+                } else {
+                    min = line;
+                    max = buffer;
+                }
+                buffer = max;
+                bw2.write(min);
+                bw2.newLine();
+            }
+            bw2.write(buffer);
+            bw2.close();
+            br2.close();
+            System.out.println(k);
+            //k++;
+        }
+        System.out.println(rows + "rows");
 
     }
 }
