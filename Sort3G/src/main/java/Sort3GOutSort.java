9f90fd687ae72340a50afb757f3cb0867b395f37
Saego <usonikru@gmail.com>
2017-01-05 15:23:39 +0200

    05/01/2017  15/22

--- Sort3G/src/main/java/Sort3GOutSort.java
+++ Sort3G/src/main/java/Sort3GOutSort.java
@@ -6,31 +6,42 @@ import java.io.*;
 public class Sort3GOutSort implements SortFile {
 
     public void sort(File source, File destination) throws IOException {
-        BufferedReader br = new BufferedReader(new FileReader(source));
-        BufferedWriter bw = new BufferedWriter(new FileWriter(destination));
-        int rows = 0;
-        String line2;
-        String line1;
-        while ((line1 = br.readLine()) != null){
-            System.out.println(line1);
-            line2 = br.readLine();
-            if (line1.length() > line2.length()) {
-                bw.write(line2);
-                bw.newLine();
-                bw.write(line1);
-                bw.newLine();
+
+//        BufferedWriter bw = new BufferedWriter(new FileWriter(destination));
+
+        //рахуємо кількість рядків
+        try(BufferedReader br = new BufferedReader(new FileReader(source))) {
+            int rows = 0;
+            while (br.readLine() != null) {
                 rows++;
+            }
+        System.out.println(rows);
+        }
+
+        //створюємо тимчасовий файл
+        File tmp = new File("tmp.txt");
+        String line;
+        String min, max;
+        BufferedReader br1 = new BufferedReader(new FileReader(source));
+        //записуємо найдовший рядок в файлі джерела останнім в тимчасовому файлі
+        String buffer = br1.readLine();
+        BufferedWriter bw1 = new BufferedWriter(new FileWriter(tmp));
+        while ((line = br1.readLine()) != null){
+            if (line.length() > buffer.length()){
+                min = buffer;
+                max = line;
             }
             else {
-                bw.write(line1);
-                bw.newLine();
-                bw.write(line2);
-                bw.newLine();
-                rows++;
+                min = line;
+                max = buffer;
             }
+            buffer = max;
+            bw1.write(min);
+            bw1.newLine();
         }
-        System.out.println(rows);
-        br.close();
-        bw.close();
+        bw1.write(buffer);
+        bw1.close();
+        br1.close();
+
     }
 }
