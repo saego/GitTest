cf2ef26bab7dd6441b3a30761cd2e72e295f0aba
Saego <usonikru@gmail.com>
2017-01-10 12:44:16 +0200

    10/01/2017  12/43 sort randomAccess

--- Sort3G/src/main/java/MainSort3G.java
+++ Sort3G/src/main/java/MainSort3G.java
@@ -1,5 +1,7 @@
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
 /**
  Created by ${Ruslan} on 04.01.17.
@@ -7,8 +9,9 @@ import java.io.IOException;
 public class MainSort3G {
     public static void main(String []args) throws IOException {
         SortFile sort = new Sort3GOutSort();
-        File source = new File("avl1.txt");
-        File destination = new File("avl2.txt");
-        sort.sort(source, destination);
+        File source = new File("test.txt");
+        sort.sort(source, new File("testRes.txt"));
+        SortFile sortRand = new Sort3GRandomAccessFile();
+        sortRand.sort(source, new File("testResRand"));
     }
 }
--- Sort3G/src/main/java/Sort3GRandomAccessFile.java
+++ Sort3G/src/main/java/Sort3GRandomAccessFile.java
@@ -1,11 +1,12 @@
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 
 /**
  Created by ${Ruslan} on 04.01.17.
  */
 public class Sort3GRandomAccessFile implements SortFile {
     public void sort(File source, File destination) throws IOException {
-
+        RandomAccessFile rafR = new RandomAccessFile(source, "r");
+        RandomAccessFile rafWR = new RandomAccessFile(destination, "rw");
+        rafR.readLine().
     }
 }
