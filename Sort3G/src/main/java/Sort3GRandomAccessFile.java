4c508f4a06a5cb12e35e833e8f2134e7eb5f9b79
Saego <usonikru@gmail.com>
2017-01-10 17:25:30 +0200

    10/01/2017  17/25 sort randomAccess

--- Sort3G/src/main/java/Sort3GRandomAccessFile.java
+++ Sort3G/src/main/java/Sort3GRandomAccessFile.java
@@ -5,8 +5,14 @@ import java.io.*;
  */
 public class Sort3GRandomAccessFile implements SortFile {
     public void sort(File source, File destination) throws IOException {
-        RandomAccessFile rafR = new RandomAccessFile(source, "r");
-        RandomAccessFile rafWR = new RandomAccessFile(destination, "rw");
-        rafR.readLine().
+        //RandomAccessFile rafR = new RandomAccessFile(source, "r");
+        //RandomAccessFile rafWR = new RandomAccessFile(destination, "rw");
+        int rows = 0;
+        try(RandomAccessFile rafR = new RandomAccessFile(source, "r")){
+            while(rafR.readLine() != null){
+                rows++;
+            }
+        }
+        System.out.println("Rows randFiwle" + rows);
     }
 }
