16c1cc10ef988a4ba07d67a28c012d033ad378a1
Saego <usonikru@gmail.com>
2017-01-11 17:50:47 +0200

    11/01/2017  17/50 sort randomAccess

--- Sort3G/src/main/java/Sort3GRandomAccessFile.java
+++ Sort3G/src/main/java/Sort3GRandomAccessFile.java
@@ -4,10 +4,25 @@ import java.io.*;
  Created by ${Ruslan} on 04.01.17.
  */
 public class Sort3GRandomAccessFile implements SortFile {
+
     private RandomAccessFile rafr;
     private RandomAccessFile rafw;
+    private RandomAccessFile rafTmp1;
+    private RandomAccessFile rafTmp2;
+
     public void sort(File source, File destination) throws IOException {
-        rafr = new RandomAccessFile(source, "r");
-        rafw = new RandomAccessFile(destination, "rw");
+        this.rafr = new RandomAccessFile(source, "r");
+        this.rafw = new RandomAccessFile(destination, "rw");
+        File tmp1 = File.createTempFile("temp1", ".txt");
+        File tmp2 = File.createTempFile("temp2", ".txt");
+        this.rafTmp1 = new RandomAccessFile(tmp1, "rw");
+        this.rafTmp2 = new RandomAccessFile(tmp2, "rw");
+        String line;
+        boolean jumper = true;
+        int lineLength = 0;
+        while ((line = rafr.readLine()) != null){
+            rafTmp1.writeBytes(String.format());
+        }
     }
+
 }
