50c0dbef22d323781df3e9cab472830527251fbf
Saego <usonikru@gmail.com>
2017-01-13 12:45:33 +0200

    13/17 12/45 palindrom start

---
+++ Palindrom/src/main/java/PalindromCheck.java
@@ -0,0 +1,12 @@
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+/**
+ Created by ${Ruslan} on 13.01.17.
+ */
+public class PalindromCheck {
+    public boolean isPalindrom(InputStream input) {
+        InputStreamReader is = new InputStreamReader(input);
+        return true;
+    }
+}
