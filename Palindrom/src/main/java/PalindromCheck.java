d411c1670847f84d8f863a11f4fe75b1831cd423
Saego <usonikru@gmail.com>
2017-01-13 13:30:41 +0200

    13/17 13/30 palindrom start

--- Palindrom/src/main/java/PalindromCheck.java
+++ Palindrom/src/main/java/PalindromCheck.java
@@ -1,3 +1,5 @@
+import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 
@@ -5,8 +7,10 @@ import java.io.InputStreamReader;
  Created by ${Ruslan} on 13.01.17.
  */
 public class PalindromCheck {
-    public boolean isPalindrom(InputStream input) {
-        InputStreamReader is = new InputStreamReader(input);
+    public boolean isPalindrom(InputStream input) throws IOException {
+        BufferedReader is = new BufferedReader(new InputStreamReader(input));
+        String word;
+        word = is.readLine();
         return true;
     }
 }
