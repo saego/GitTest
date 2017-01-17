9d6e59c81b1c7f10dec208a47c5f9c2ca39cdf34
Saego <usonikru@gmail.com>
2017-01-17 18:21:19 +0200

    17/17 18/20 start chat

--- Chat/src/main/java/MainChat.java
+++ Chat/src/main/java/MainChat.java
@@ -1,9 +1,20 @@
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 /**
  Created by ${Ruslan} on 17.01.17.
  */
 public class MainChat {
+    public static void main(String[] args) throws FileNotFoundException {
     WorkChat chat = new WorkChat();
     File answers = new File("answers.txt");
+    File logs = new File("logs.txt");
+    try {
+        chat.chatting(answers, logs, System.in, System.out);
+    }
+    catch (IOException ex){
+        System.out.println(ex.getMessage());
+    }
+    }
 }
--- Chat/src/main/java/WorkChat.java
+++ Chat/src/main/java/WorkChat.java
@@ -3,13 +3,23 @@ import java.io.*;
 /**
  Created by ${Ruslan} on 16.01.17.
  */
-public class WorkChat {
+class WorkChat {
     private RandomAccessFile rafr;
-    private RandomAccessFile rfrw;
-    public void chatting(File fileAnswers, File logs, InputStream input, OutputStream output) throws FileNotFoundException {
+    private RandomAccessFile rafrw;
+    void chatting(File fileAnswers, File logs, InputStream input, OutputStream output) throws IOException {
         rafr = new RandomAccessFile(fileAnswers, "r");
-        rfrw = new RandomAccessFile(logs, "rw");
+        rafrw = new RandomAccessFile(logs, "rw");
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(input));
         BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(output));
+        String lineAsk = bufferedReader.readLine();
+        bufferedReader.close();
+        rafrw.writeBytes(lineAsk);
+        String lineAnswer = rafr.readLine();
+        rafr.close();
+        bufferedWriter.write(lineAnswer.concat(String.));
+        bufferedWriter.close();
+        rafrw.writeBytes(lineAnswer);
+        rafrw.close();
+
     }
 }
