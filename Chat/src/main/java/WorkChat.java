3e49f85fa0ac333616e9890c2d74e39e736b7569
Saego <usonikru@gmail.com>
2017-01-18 11:12:30 +0200

    18/17 11/12 start chat

--- Chat/src/main/java/WorkChat.java
+++ Chat/src/main/java/WorkChat.java
@@ -4,22 +4,25 @@ import java.io.*;
  Created by ${Ruslan} on 16.01.17.
  */
 class WorkChat {
-    private RandomAccessFile rafr;
-    private RandomAccessFile rafrw;
+
     void chatting(File fileAnswers, File logs, InputStream input, OutputStream output) throws IOException {
-        rafr = new RandomAccessFile(fileAnswers, "r");
-        rafrw = new RandomAccessFile(logs, "rw");
+        RandomAccessFile rafr = new RandomAccessFile(fileAnswers, "r");
+        RandomAccessFile rafrw = new RandomAccessFile(logs, "rw");
+        String lineAsk, lineAnswer;
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(input));
         BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(output));
-        String lineAsk = bufferedReader.readLine();
+        //do {
+        lineAsk = bufferedReader.readLine();
         bufferedReader.close();
-        rafrw.writeBytes(lineAsk);
-        String lineAnswer = rafr.readLine();
+        rafrw.writeBytes(lineAsk.concat(System.lineSeparator()));
+        lineAnswer = rafr.readLine();
         rafr.close();
-        bufferedWriter.write(lineAnswer.concat(String.));
+        bufferedWriter.write(lineAnswer);
+        bufferedWriter.newLine();
         bufferedWriter.close();
         rafrw.writeBytes(lineAnswer);
         rafrw.close();
-
+        //}
+        //while (!lineAsk.equals("Stop"));
     }
 }
