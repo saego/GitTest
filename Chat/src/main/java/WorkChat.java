a6725903f881a0e0087bb9ace084f7e986ac09b8
Saego <usonikru@gmail.com>
2017-01-19 15:11:54 +0200

    19/17 12/01 chat don't know what to do

--- Chat/src/main/java/WorkChat.java
+++ Chat/src/main/java/WorkChat.java
@@ -6,31 +6,57 @@ import java.io.*;
 class WorkChat {
 
     void chatting(File fileAnswers, File logs, InputStream input) throws IOException {
-            String lineAsk, lineAnswer;
             BufferedReader bufferedReader;
-            //BufferedWriter bufferedWriter;
+            int []positions;
+            int n = 0, rowsQuantity =0;
+            try(RandomAccessFile rafr00 = new RandomAccessFile(fileAnswers, "r")){
+                while (rafr00.readLine() != null){
+                    rowsQuantity++;
+                }
+            }
+            catch (IOException ex){
+                System.out.println(ex.getMessage());
+            }
+            positions = new int[rowsQuantity];
+//формуємо массив позицій початків рядків
+        try (RandomAccessFile rafr0 = new RandomAccessFile(fileAnswers, "r")){
+            do {
+                long m = rafr0.getFilePointer();
+                if (n != rowsQuantity - 1){
+                positions[n] = (int)m;
+                n++;
+                }
+            }
+            while (rafr0.readLine() != null);
+        }
+        catch (IOException ex){
+            System.out.println(ex.getMessage());
+        }
+
         try(
             RandomAccessFile rafr = new RandomAccessFile(fileAnswers, "r");
             BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(logs))
         ) {
+            String lineAsk;
             do
             {
 //зчитуємо з консолі питання
                 bufferedReader = new BufferedReader(new InputStreamReader(input));
                 lineAsk = bufferedReader.readLine();
 //записуємо в лог-файл питання
-                bufferedWriter.write(lineAsk);
+                String user = "User     : ";
+                bufferedWriter.write(user.concat(lineAsk));
 //зчитуємо відповідь з файла відповідей
-                if(!lineAsk.equals("stop")) {
+                if (!lineAsk.equals("stop"))
                     bufferedWriter.newLine();
-                    lineAnswer = rafr.readLine();
+                if(!lineAsk.equals("stop") && !lineAsk.equals("pause")) {
+                    rafr.seek((long)positions[(int) (Math.random() * (positions.length - 1))]);
+                    String lineAnswer = rafr.readLine();
 //записуємо в лог-файл відповідь
-                    bufferedWriter.write(lineAnswer.concat(System.lineSeparator()));
+                    String computer = "Computer : ";
+                    bufferedWriter.write(computer.concat(lineAnswer).concat(System.lineSeparator()));
 //записуємо відповідь в консоль
-                    //bufferedWriter = new BufferedWriter(new OutputStreamWriter(output));
-                    //bufferedWriter.write(lineAnswer);
-                    System.out.println(lineAnswer);
-                    //bufferedWriter.newLine();
+                    System.out.println(computer.concat(lineAnswer));
                 }
             }
             while (!lineAsk.equals("stop"));
