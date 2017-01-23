8e86cf64d9f9ec664f2fd6713b2336a634d122e8
Saego <usonikru@gmail.com>
2017-01-23 11:28:41 +0200

    23/01/17 11/25 example socket

---
+++ SocketClientServer/src/main/java/ClientApp/Client.java
@@ -0,0 +1,42 @@
+package ClientApp;
+
+import java.io.*;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ Created by ${Ruslan} on 23.01.17.
+ */
+public class Client {
+    public static void main(String []args) {
+        int serverPort = 3000;
+        String internetAdress = "127.0.0.1";
+        try {
+            InetAddress inetAddress = InetAddress.getByName(internetAdress);
+            System.out.println("Connecting to server...");
+            Socket socket = new Socket(inetAddress, serverPort);
+
+            InputStream inputStream = socket.getInputStream();
+            OutputStream outputStream = socket.getOutputStream();
+
+            DataInputStream in = new DataInputStream(inputStream);
+            DataOutputStream out = new DataOutputStream(outputStream);
+
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+            String string = null;
+            System.out.println("Input message to server: ");
+            while (true){
+                string = bufferedReader.readLine();
+                out.writeUTF(string);
+                out.flush();
+                string = in.readUTF();
+                System.out.println("Server sent back: " + string);
+                System.out.println("Input next message to server: ");
+            }
+        }
+        catch (Exception ex){
+            ex.printStackTrace();
+        }
+    }
+}
---
+++ SocketClientServer/src/main/java/ServerApp/ClientApp/Server.java
@@ -0,0 +1,41 @@
+package ServerApp.ClientApp;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ Created by ${Ruslan} on 23.01.17.
+ */
+public class Server {
+    public static void main(String []args){
+        int port = 3000;
+        try {
+            ServerSocket serverSocket = new ServerSocket(port);
+            System.out.println("Waiting for server connection...");
+            Socket socket = serverSocket.accept();
+            System.out.println("We've got connection");
+
+            InputStream inputStream = socket.getInputStream();
+            OutputStream outputStream = socket.getOutputStream();
+
+            DataInputStream in = new DataInputStream(inputStream);
+            DataOutputStream out = new DataOutputStream(outputStream);
+
+            String string = null;
+            while (true){
+                string = in.readUTF();
+                System.out.println("Message: " + string);
+                System.out.println("Send back...");
+                out.writeUTF(string);
+                out.flush();
+            }
+        }
+        catch (Exception ex){
+            ex.printStackTrace();
+        }
+    }
+}
