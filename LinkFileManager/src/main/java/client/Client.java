e75c173082bfd71b72869d0c56669aba31b897ae
Saego <usonikru@gmail.com>
2017-01-24 15:39:21 +0200

    24/01/17 15/39 start file manager

--- LinkFileManager/src/main/java/client/Client.java
+++ LinkFileManager/src/main/java/client/Client.java
@@ -1,6 +1,9 @@
 package client;
 
+import java.io.BufferedReader;
 import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
 import java.net.InetAddress;
 import java.net.Socket;
 
@@ -16,7 +19,14 @@ public class Client {
             System.out.println("Connection to server...");
             Socket socket = new Socket(inetAddress, port);
 
-            DataInputStream dataInputStream = new DataInputStream(socket.getInputStream());
+            //DataInputStream dataInputStream = new DataInputStream(socket.getInputStream());
+            DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream());
+
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+            String word = bufferedReader.readLine();
+            dataOutputStream.writeUTF(word);
+            dataOutputStream.flush();
         }
         catch (Exception x){
             x.printStackTrace();
--- LinkFileManager/src/main/java/server/Server.java
+++ LinkFileManager/src/main/java/server/Server.java
@@ -1,5 +1,7 @@
 package server;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.InputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -10,16 +12,20 @@ import java.net.Socket;
 public class Server {
     public static void main(String []args){
     int port  = 1092;
-    try {
+        try {
         ServerSocket ss = new ServerSocket(port);
         System.out.println("Waiting for connection...");
         Socket socket = ss.accept();
         System.out.println("Accept connetion!");
 
-        InputStream inputStream = socket.getInputStream();
-    }
-    catch (Exception x){
+        DataInputStream dataInputStream = new DataInputStream(socket.getInputStream());
+        //DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream());
+
+        String word = dataInputStream.readUTF();
+        System.out.println(word);
+        }
+        catch (Exception x){
         x.printStackTrace();
-    }
+        }
     }
 }
