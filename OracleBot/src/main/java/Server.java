ade60f38db3a110f139da8424eb0571fc3f46f28
Saego <usonikru@gmail.com>
2017-03-30 18:15:27 +0300

    17/47 30.03.2017 start

--- OracleBot/src/main/java/Server.java
+++ OracleBot/src/main/java/Server.java
@@ -1,4 +1,6 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -11,5 +13,6 @@ public class Server {
         int port = 1248;
         Socket socket = new ServerSocket(port).accept();
         PrintWriter pw = new PrintWriter(socket.getOutputStream(), true);
+        BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
     }
 }
