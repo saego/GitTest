52efc9e67a49d5bd6249c983e76f841225caaf9d
Saego <usonikru@gmail.com>
2017-04-04 17:01:34 +0300

    17/01 04.04.2017 start

--- OracleBot/src/main/java/Client.java
+++ OracleBot/src/main/java/Client.java
@@ -1,8 +1,14 @@
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.Socket;
+
 /**
  Created by Saego on 29.03.2017.
  */
 public class Client {
-    public static void main(String []args){
-
+    public static void main(String []args) throws IOException {
+        int port  = 1248;
+        String internetAddress = "127.0.0.1";
+        Socket socket = new Socket(InetAddress.getByName(internetAddress), port);
     }
 }
