45dde9d77de9b75a8e6218a243e3f9c483b1de5b
Saego <usonikru@gmail.com>
2017-04-12 23:55:32 +0300

    23-55 12-04-2017 start

--- FileSearcher/src/main/java/Searcher.java
+++ FileSearcher/src/main/java/Searcher.java
@@ -5,23 +5,47 @@ import java.util.Scanner;
  Created by Saego on 11.04.2017.
  */
 public class Searcher {
-    private String directory;
+    private String searchDirectory;
     private String extension;
-    FileProp fileProp = new FileProp(directory, extension);
-    private File searchDirectory;
 
-    public boolean isExist(String directory){
-        return (searchDirectory = new File(directory)).exists() && searchDirectory.isDirectory();
+    private Searcher(String searchDirectory, String extension) {
+        this.searchDirectory = searchDirectory;
+        this.extension = extension;
     }
 
 
+    private boolean isExist(){
+        File sDirectory;
+        return (sDirectory = new File(this.searchDirectory)).exists() && sDirectory.isDirectory();
+    }
+
+    private void fileWalker(File file){
+        //File file = new File(this.searchDirectory);
+        File []folders = file.listFiles();
+        assert folders != null;
+        for (File folder:
+             folders) {
+            if (folder.isDirectory()){
+                fileWalker(folder);
+            }
+            else {
+                System.out.printf("File: [%s]", folder.getAbsolutePath());
+            }
+        }
+    }
 
     public static void main(String []args){
         Scanner scanner = new Scanner(System.in);
         System.out.println("Input directory");
         String directory = scanner.nextLine();
         System.out.println("Input extension");
-        String extensoin = scanner.nextLine();
-        Searcher searcher = new Searcher();
+        String extension = scanner.nextLine();
+        Searcher searcher = new Searcher(directory, extension);
+        if (searcher.isExist()){
+            searcher.fileWalker(new File(directory));
+        }
+        else {
+            System.out.println("Directory wasn't found");
+        }
     }
 }
