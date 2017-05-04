13f285e44b41ea12f1acfa040d8810a71578c802
Saego <usonikru@gmail.com>
2017-05-04 10:55:45 +0300

    10/55 04/05/17 start SOLID calc

--- 1/ClientCalculator/src/main/java/Calculator.java
+++ 1/ClientCalculator/src/main/java/Calculator.java
@@ -3,16 +3,43 @@
  */
 public class Calculator {
     /**
-     * Result value.
+     * Result of calculation value.
      */
-    double result;
+    private double result;
 
     /**
-     * Addition values
+     * Addition values.
      * @param first
      * @param second
      */
     public void add(double first, double second){
         this.result = first + second;
+    }
+
+    /**
+     * Multiply values.
+      * @param first
+     * @param second
+     */
+    public void multiply(double first, double second){
+        this.result = first * second;
+    }
+
+    /**
+     * Divide values.
+      * @param first
+     * @param second
+     */
+    public void div(double first, double second){
+        this.result = first / second;
+    }
+
+    /**
+     * Subscription values.
+     * @param first
+     * @param second
+     */
+    public void sub(double first, double second){
+        this.result = first - second;
     }
 }
