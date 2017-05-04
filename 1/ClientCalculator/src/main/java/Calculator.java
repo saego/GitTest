ea19e55e8ab37cb716259b53719d4860bbc21254
Saego <usonikru@gmail.com>
2017-05-04 10:48:35 +0300

    10/47 04/05/17 start SOLID calc

---
+++ 1/ClientCalculator/src/main/java/Calculator.java
@@ -0,0 +1,18 @@
+/**
+ * Created by Saego on 04.05.2017.
+ */
+public class Calculator {
+    /**
+     * Result value.
+     */
+    double result;
+
+    /**
+     * Addition values
+     * @param first
+     * @param second
+     */
+    public void add(double first, double second){
+        this.result = first + second;
+    }
+}
