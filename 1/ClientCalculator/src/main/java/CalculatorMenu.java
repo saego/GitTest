06fad961939055ba73ba6fd5f44b2a365091b5f4
Saego <usonikru@gmail.com>
2017-05-04 13:09:10 +0300

    13/09 04/05/17 start SOLID calc

---
+++ 1/ClientCalculator/src/main/java/CalculatorMenu.java
@@ -0,0 +1,5 @@
+/**
+ * Created by Saego on 04.05.2017.
+ */
+public class CalculatorMenu {
+}
---
+++ 1/ClientCalculator/src/main/java/Input.java
@@ -0,0 +1,11 @@
+/**
+ *Created by Saego on 04.05.2017.
+ */
+public interface Input {
+    /**
+     * Dialog with customer.
+     * @param question - question.
+     * @return - answer.
+     */
+    String ask(String question);
+}
