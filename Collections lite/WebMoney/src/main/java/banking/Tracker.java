f9ec3067fe8eda1cc0092626bf8b12962b05b6d8
Saego <usonikru@gmail.com>
2017-06-14 23:20:53 +0300

    23:20 14.06.2017

--- Collections lite/WebMoney/src/main/java/banking/Tracker.java
+++ Collections lite/WebMoney/src/main/java/banking/Tracker.java
@@ -3,6 +3,7 @@ package banking;
 import clientData.Account;
 import clientData.User;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -12,9 +13,41 @@ import java.util.Map;
  */
 class Tracker {
 
-    public Map<User, List<Account>> userListMap = new HashMap<User, List<Account>>();
+    private Map<User, List<Account>> userListMap = new HashMap<User, List<Account>>();
 
-    public void addClient(User user, List<Account> accounts){
-        userListMap.put(user, accounts);
+    public void addClient(User user){
+        userListMap.put(user, new ArrayList<Account>());
+    }
+
+    public Map<User, List<Account>> getAllClients(){
+        return this.userListMap;
+    }
+
+    public void addAccount(User user, Account account){
+        this.userListMap.get(user).add(account);
+    }
+
+    public void removeClient(User user){
+        this.userListMap.remove(user);
+    }
+
+    public void removeAccount(User user, Account account){
+        this.userListMap.get(user).remove(account);
+    }
+
+    public boolean transferMoney(User sourceUsr, Account sourceAcc, User destinationUsr, Account destinationAcc, int amountOfTransfer){
+        boolean resultOfTransfer = false;
+        if (this.userListMap.get(sourceUsr).contains(sourceAcc) && this.userListMap.get(destinationUsr).contains(destinationAcc)) {
+            if (this.userListMap.get(sourceUsr).get(this.userListMap.get(sourceUsr).indexOf(sourceAcc)).getMoneyValue() >= amountOfTransfer){
+                this.userListMap.get(sourceUsr).get(this.userListMap.get(sourceUsr).indexOf(sourceAcc)).setMoneyValue(
+                        this.userListMap.get(sourceUsr).get(this.userListMap.get(sourceUsr).indexOf(sourceAcc)).getMoneyValue() - amountOfTransfer
+                );
+                this.userListMap.get(destinationUsr).get(this.userListMap.get(destinationUsr).indexOf(destinationAcc)).setMoneyValue(
+                        this.userListMap.get(destinationUsr).get(this.userListMap.get(destinationUsr).indexOf(destinationAcc)).getMoneyValue() + amountOfTransfer
+                );
+                resultOfTransfer = true;
+            }
+        }
+        return resultOfTransfer;
     }
 }
