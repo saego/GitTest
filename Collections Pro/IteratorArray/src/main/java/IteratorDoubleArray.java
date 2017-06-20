6609d6dd1842ef0be0b56dcb7f8e05c090ce00ac
Saego <usonikru@gmail.com>
2017-06-20 22:59:05 +0300

    22:58 20.06.2017

---
+++ Collections Pro/IteratorArray/src/main/java/IteratorDoubleArray.java
@@ -0,0 +1,35 @@
+import java.util.Iterator;
+
+/**
+ Created by Saego on 19.06.2017.
+ */
+public class IteratorDoubleArray implements Iterator{
+    private final int [][] array;
+    private int iteratorI = 0;
+    private int iteratorJ = 0;
+
+    IteratorDoubleArray(int[][] array) {
+        this.array = array;
+    }
+
+
+    public boolean hasNext() {
+        return (array[iteratorI].length > iteratorJ) && (array.length > iteratorI);
+    }
+
+    public Object next() {
+        Object obj = null;
+        if ((iteratorJ != array[iteratorI].length)){
+            obj = array[iteratorI][iteratorJ];
+            iteratorJ++;
+        }
+        if ((iteratorI != array.length) && (iteratorJ == array[iteratorI].length)){
+        //else{
+            iteratorJ = 0;
+            iteratorI++;
+            //obj = null;
+            //next();
+        }
+        return obj;
+    }
+}
---
+++ Collections Pro/IteratorArray/src/test/java/IteratorDoubleArrayTest.java
@@ -0,0 +1,41 @@
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+/**
+ Created by Saego on 20.06.2017.
+ */
+public class IteratorDoubleArrayTest {
+    private int[][] array = new int[][]{{1, 2, 3}, {4, 5, 6}};
+    private IteratorDoubleArray iteratorDoubleArray = new IteratorDoubleArray(array);
+    @Test
+    public void hasNext() throws Exception {
+        iteratorDoubleArray.next();
+        iteratorDoubleArray.next();
+        assertThat(iteratorDoubleArray.hasNext(), is(true));
+        iteratorDoubleArray.next();
+        iteratorDoubleArray.next();
+        iteratorDoubleArray.next();
+        iteratorDoubleArray.next();
+        //assertThat(iteratorDoubleArray.hasNext(), is(false));
+        //assertThat(iteratorDoubleArray.hasNext(), is(false));
+    }
+
+    @Test
+    public void next() throws Exception {
+        int res = (Integer) iteratorDoubleArray.next();
+        assertThat(res, is(1));
+        res = (Integer)iteratorDoubleArray.next();
+        assertThat(res, is(2));
+        res = (Integer)iteratorDoubleArray.next();
+        assertThat(res, is(3));
+        res = (Integer)iteratorDoubleArray.next();
+        assertThat(res, is(4));
+        res = (Integer)iteratorDoubleArray.next();
+        assertThat(res, is(5));
+        res = (Integer)iteratorDoubleArray.next();
+        assertThat(res, is(6));
+    }
+
+}
