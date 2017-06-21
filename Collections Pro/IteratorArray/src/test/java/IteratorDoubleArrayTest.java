d5c9a851c12cd22d20e2e784b74996acd40ffc87
Saego <usonikru@gmail.com>
2017-06-21 09:36:40 +0300

    09:36 21.06.17

--- Collections Pro/IteratorArray/src/test/java/IteratorDoubleArrayTest.java
+++ Collections Pro/IteratorArray/src/test/java/IteratorDoubleArrayTest.java
@@ -7,7 +7,7 @@ import static org.junit.Assert.*;
  Created by Saego on 20.06.2017.
  */
 public class IteratorDoubleArrayTest {
-    private int[][] array = new int[][]{{1, 2, 3}, {4, 5, 6}};
+    private int[][] array = new int[][]{{1, 2, 3}, {4, 5, 6}, {7}};
     private IteratorDoubleArray iteratorDoubleArray = new IteratorDoubleArray(array);
     @Test
     public void hasNext() throws Exception {
@@ -16,6 +16,8 @@ public class IteratorDoubleArrayTest {
         assertThat(iteratorDoubleArray.hasNext(), is(true));
         iteratorDoubleArray.next();
         iteratorDoubleArray.next();
+        iteratorDoubleArray.next();
+        assertThat(iteratorDoubleArray.hasNext(), is(true));
         iteratorDoubleArray.next();
         assertThat(iteratorDoubleArray.hasNext(), is(true));
         iteratorDoubleArray.next();
@@ -37,6 +39,8 @@ public class IteratorDoubleArrayTest {
         assertThat(res, is(5));
         res = (Integer)iteratorDoubleArray.next();
         assertThat(res, is(6));
+        res =(Integer)iteratorDoubleArray.next();
+        assertThat(res, is(7));
     }
 
 }
