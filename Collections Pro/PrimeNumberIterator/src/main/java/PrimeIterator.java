49b63f1a8c4e6d094039bfe9b429f3bc69c40198
Saego <usonikru@gmail.com>
2017-06-23 16:15:18 +0300

    14:46 23.06.17

--- Collections Pro/PrimeNumberIterator/src/main/java/PrimeIterator.java
+++ Collections Pro/PrimeNumberIterator/src/main/java/PrimeIterator.java
@@ -8,10 +8,19 @@ public class PrimeIterator implements Iterator{
     private final int[] array;
     private int iterator = 0;
 
+    /**
+     * Constructor.
+     * @param array - input array.
+     */
     PrimeIterator(int[] array) {
         this.array = array;
     }
 
+    /**
+     * Check current value is prime.
+     * @param n - value of array cell.
+     * @return - if divide only by 1 and n.
+     */
     private boolean checkDivNumbers(int n){
         boolean tmpRes = true;
         for (int i = 2; i <= n / 2; i++){
@@ -23,6 +32,10 @@ public class PrimeIterator implements Iterator{
         return tmpRes;
     }
 
+    /**
+     * Check if is prime number forward.
+     * @return - if has prime forward.
+     */
     private boolean ifHasNextPrime(){
         boolean result = false;
         for (int i = this.iterator; i < this.array.length; i++){
@@ -35,10 +48,18 @@ public class PrimeIterator implements Iterator{
         return result;
     }
 
+    /**
+     *
+     * @return - if has next.
+     */
     public boolean hasNext() {
         return (this.array.length > this.iterator) && (this.ifHasNextPrime());
     }
 
+    /**
+     * Move cursor to next position.
+     * @return next prime value.
+     */
     public Object next() {
         if (this.ifHasNextPrime()) {
             return this.array[iterator++];
