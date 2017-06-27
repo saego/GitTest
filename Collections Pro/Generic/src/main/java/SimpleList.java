a9bd8221822c631e0186bb4ebf61042c49ac9792
Saego <usonikru@gmail.com>
2017-06-27 15:47:11 +0300

    15:46 27.06.2017

---
+++ Collections Pro/Generic/src/main/java/SimpleList.java
@@ -0,0 +1,15 @@
+/**
+ *Created by Saego on 27.06.2017.
+ */
+public class SimpleList <E>{
+
+    Object[] objects;
+
+    public SimpleList(int size) {
+        this.objects = new Object[size];
+    }
+
+    public void add(){
+
+    }
+}
