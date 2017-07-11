3af49c59bdd91fded2b9a26a95cc62addf0fea90
Saego <usonilru@gmail.com>
2017-07-11 16:37:00 +0300

    16:36 11.07.2017

--- Collections Pro/Generic/src/test/java/MyStack/MyStackTest.java
+++ Collections Pro/Generic/src/test/java/MyStack/MyStackTest.java
@@ -1,5 +1,7 @@
 package MyStack;
 
+import SimpleList.Person;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -8,8 +10,20 @@ import static org.junit.Assert.*;
  * Created by Saego on 11.07.2017.
  */
 public class MyStackTest {
+    private MyStack <Person> persons = new MyStack<Person>();
+    private MyStack<Person> testPersons = new MyStack<Person>();
+
+    @Before
+    public void initialization(){
+        this.testPersons.push(new Person("Irena", 18));
+        this.testPersons.push(new Person("Johnathan", 4));
+        this.testPersons.push(new Person("Hanna", 26));
+        this.testPersons.push(new Person("Jack", 32));
+        this.testPersons.push(new Person("Bob", 8));
+    }
     @Test
     public void push() throws Exception {
+        persons
     }
 
     @Test
