b914c6b5e234abd3c5a44bae62f78f2ef55007d9
Saego <usonilru@gmail.com>
2017-07-12 10:14:36 +0300

    10:13 12.07.2017

--- Collections Pro/Generic/src/test/java/MyStack/MyStackTest.java
+++ Collections Pro/Generic/src/test/java/MyStack/MyStackTest.java
@@ -4,6 +4,9 @@ import SimpleList.Person;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.Stack;
+
+import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 
 /**
@@ -12,6 +15,7 @@ import static org.junit.Assert.*;
 public class MyStackTest {
     private MyStack <Person> persons = new MyStack<Person>();
     private MyStack<Person> testPersons = new MyStack<Person>();
+    private Stack<Person> realStack = new Stack<Person>();
 
     @Before
     public void initialization(){
@@ -23,15 +27,45 @@ public class MyStackTest {
     }
     @Test
     public void push() throws Exception {
-        persons
+        this.persons.push(new Person("Irena", 18));
+        this.persons.push(new Person("Johnathan", 4));
+        this.persons.push(new Person("Hanna", 26));
+        this.persons.push(new Person("Jack", 32));
+        this.persons.push(new Person("Bob", 8));
+        this.realStack.push(new Person("Irena", 18));
+        this.realStack.push(new Person("Johnathan", 4));
+        this.realStack.push(new Person("Hanna", 26));
+        this.realStack.push(new Person("Jack", 32));
+        this.realStack.push(new Person("Bob", 8));
+        Person[] array1 = new Person[5];
+        Person[] array2 = new Person[5];
+        int i = 0;
+        for (Person person:
+             this.persons) {
+            array1[i] = person;
+            i++;
+        }
+        int k  = 0;
+        for (Person stackPerson:
+             this.realStack) {
+            array2[k] = stackPerson;
+            i++;
+        }
+        assertThat(array1, is(array2));
     }
 
     @Test
     public void pop() throws Exception {
+        assertThat(this.testPersons.pop(), is(new Person("Bob", 8)));
     }
 
     @Test
     public void peek() throws Exception {
+        assertThat(this.testPersons.pop(), is(new Person("Bob", 8)));
+        assertThat(this.testPersons.peek(), is(new Person("Bob", 8)));
+        System.out.println(this.testPersons.pop());
+        assertThat(this.testPersons.pop(), is(new Person("Jack", 12)));
+        assertThat(this.testPersons.peek(), is(new Person("Bob", 8)));
     }
 
     @Test
