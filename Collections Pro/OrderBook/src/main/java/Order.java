1612f49767e31ba3cbba7c5dd3751187881c97f8
Saego <usonikru@gmail.com>
2017-09-04 12:20:49 +0300

    12:20 04.09.2017

--- Collections Pro/OrderBook/src/main/java/Order.java
+++ Collections Pro/OrderBook/src/main/java/Order.java
@@ -2,4 +2,15 @@
  * Created by Saego on 04.09.2017.
  */
 public class Order {
+    private float price;
+    private String type;
+    private int quantity;
+    private int id;
+
+    public Order(float price, String type, int quantity, int id) {
+        this.price = price;
+        this.type = type;
+        this.quantity = quantity;
+        this.id = id;
+    }
 }
