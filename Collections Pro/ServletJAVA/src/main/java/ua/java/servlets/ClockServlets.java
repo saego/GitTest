9057f56027657b8dae87fcce82f9d404910d5837
Saego <usonikru@gmail.com>
2017-10-09 11:13:28 +0300

    09.10.2017 11:13

---
+++ Collections Pro/ServletJAVA/src/main/java/ua/java/servlets/ClockServlets.java
@@ -0,0 +1,20 @@
+package ua.java.servlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
+
+/**
+ * Created by Saego on 09.10.2017.
+ */
+public class ClockServlets extends HttpServlet{
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        PrintWriter out = response.getWriter();
+        out.println("Current time : " + new Date());
+    }
+}
