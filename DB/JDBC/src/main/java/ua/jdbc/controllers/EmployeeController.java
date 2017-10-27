471a08faf705696681ea679cbc72b26d200fdb75
Saego <usonikru@gmail.com>
2017-10-27 20:54:51 +0300

    20:54 27-10-2017 with spring beans

---
+++ DB/JDBC/src/main/java/ua/jdbc/controllers/EmployeeController.java
@@ -0,0 +1,36 @@
+package ua.jdbc.controllers;
+
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionDefinition;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
+import ua.jdbc.jdbc.DAOjdbc;
+import ua.jdbc.source.Employee;
+
+import java.util.List;
+
+public class EmployeeController {
+    private PlatformTransactionManager txManager;
+    private DAOjdbc daOjdbc;
+
+    public List<Employee> getAll(){
+        TransactionStatus status = txManager.getTransaction(new DefaultTransactionDefinition(TransactionDefinition.PROPAGATION_REQUIRED));
+        try {
+            List<Employee> employees = daOjdbc.getAll();
+            txManager.commit(status);
+            return employees;
+        }
+        catch (Exception e){
+            txManager.rollback(status);
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void setTxManager(PlatformTransactionManager txManager) {
+        this.txManager = txManager;
+    }
+
+    public void setDaOjdbc(DAOjdbc daOjdbc) {
+        this.daOjdbc = daOjdbc;
+    }
+}
